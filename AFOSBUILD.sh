87e8fa059cce22acc99baf18f60349f6c104eaf0
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-16 17:10:34 -0300

    Add andraxbin

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,3 +1,7 @@
+rm -rf /opt/ANDRAX/bin/fierce.py
+cp -Rf andraxbin/fierce.py /opt/ANDRAX/bin/fierce.py
+chmod 755 /opt/ANDRAX/bin/fierce.py
+
 rm -rf /opt/ANDRAX/fierce-python3/
 
 cp -Rf $(pwd)/fierce/ /opt/ANDRAX/fierce-python3
---
+++ andraxbin/fierce.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/ANDRAX/fierce-python3/
+
+python3 /opt/ANDRAX/fierce-python3/fierce.py "$@"
